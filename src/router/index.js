403d427712a5ec3ee8a6e95abf5de9a03f0698fa
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2026-02-02 16:30:20 +0200

    feat: implement SPA routing and add custom 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', name: 'home', component: () => import('../pages/HomePage.vue') },
@@ -23,7 +23,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior() {
     return { top: 0 }
